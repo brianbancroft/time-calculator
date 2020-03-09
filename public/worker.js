46ccc430c68ef4b57816a78a25651778ce7026dc
Brian Bancroft <hello@brianbancroft.ca>
2020-03-09 09:26:40 -0700

    🔧  Use full url to cache

--- public/worker.js
+++ public/worker.js
@@ -1,5 +1,5 @@
 var CACHE_NAME = 'pwa-task-manager'
-var urlsToCache = ['/', '/completed']
+var urlsToCache = ['https://timesheet-calculator.netlify.com/']
 
 // Install a service worker
 self.addEventListener('install', event => {
