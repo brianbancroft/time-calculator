80763a1dabd634c41dbbe4406111c4a766445be0
Brian Bancroft <hello@brianbancroft.ca>
2020-03-15 11:18:45 -0700

    🚧   Add scaffolds for tests

--- src/components/TimeRecords/TimeRecords.test.js
+++ src/components/TimeRecords/TimeRecords.test.js
@@ -5,9 +5,19 @@ import { render } from '@testing-library/react'
 import TimeRecords from './TimeRecords'
 
 test('TimeRecords component renders with the expected UI elements', () => {
-const { getByText } = render(
-<TimeRecords />)
+  const { getByText } = render(<TimeRecords />)
 
-expect(getByText('I should fail'))
+  expect(getByText('I should fail'))
+})
 
+describe('Add record button ', () => {
+  it('adds a record when pushed', () => {})
+})
+
+describe('component rendering', () => {
+  it('renders the correct number of components', () => {})
+})
+
+describe('time calculation', () => {
+  it('correctly adds the number of stuff ', () => {})
 })
